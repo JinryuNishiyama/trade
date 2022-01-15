866c5f64e2bc0c2494a5246768a13194bc4e4c76
西山仁隆 <jinryu@NishiyamaJinryu-MacBookPro.local>
2022-01-15 21:26:41 +0900

    Add: トップページのテストを追加
    ページが正しく表示されることとページ遷移できることを確認するために、追加しました。

--- spec/system/games_spec.rb
+++ spec/system/games_spec.rb
@@ -12,6 +12,44 @@ RSpec.describe "Games", type: :system do
       it "ゲーム名が表示されること" do
         expect(page).to have_content game.name
       end
+
+      it "ヘッダー内に「ログイン」・「新規登録」が表示され、「ログアウト」が表示されないこと" do
+        within "header" do
+          expect(page).to have_content "ログイン"
+          expect(page).to have_content "新規登録"
+          expect(page).not_to have_content "ログアウト"
+        end
+      end
+    end
+
+    describe "ページ遷移テスト" do
+      it "ヘッダー内の「ログイン」をクリックすると、ログインページに遷移すること" do
+        within "header" do
+          click_on "ログイン"
+        end
+        expect(current_path).to eq new_user_session_path
+      end
+
+      it "ヘッダー内の「新規登録」をクリックすると、アカウント登録ページに遷移すること" do
+        within "header" do
+          click_on "新規登録"
+        end
+        expect(current_path).to eq new_user_registration_path
+      end
+
+      it "「アカウント登録はこちらから」の「こちら」をクリックすると、アカウント登録ページに遷移すること" do
+        within ".note-registration" do
+          click_on "こちら"
+        end
+        expect(current_path).to eq new_user_registration_path
+      end
+
+      it "「ログインはこちらから」の「こちら」をクリックすると、ログインページに遷移すること" do
+        within ".note-login" do
+          click_on "こちら"
+        end
+        expect(current_path).to eq new_user_session_path
+      end
     end
   end
 end
