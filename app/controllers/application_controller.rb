37afb712d03b44ef6f07f4d929ac90aeef44c7cc
西山仁隆 <jinryu@NishiyamaJinryu-MacBookPro.local>
2022-01-16 20:43:47 +0900

    Add: deviseのコントローラーにユーザー情報編集時のストロングパラメータを追加
    ユーザー情報編集時にエラーが出ないようにするため、追加しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :icon, :remove_icon])
   end
 end
