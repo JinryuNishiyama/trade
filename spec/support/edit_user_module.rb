f5a82df3ce74cb6456f847bbab570b8318fa1116
西山仁隆 <jinryu@NishiyamaJinryu-MacBookPro.local>
2022-01-19 17:54:26 +0900

    Add: ユーザー情報編集処理をモジュールに切り出し
    テストで複数回使用するため、追加しました。

---
+++ spec/support/edit_user_module.rb
@@ -0,0 +1,27 @@
+module EditUserModule
+  def configure_icon(user)
+    visit edit_user_registration_path
+    within ".user-edit-form" do
+      fill_in "名前", with: user.name
+      attach_file "アイコン画像", "#{Rails.root}/spec/factories/test.jpeg"
+      fill_in "メールアドレス", with: user.email
+      fill_in "新しいパスワード", with: ""
+      fill_in "新しいパスワード（確認用）", with: ""
+      fill_in "現在のパスワード", with: user.password
+      click_on "更新"
+    end
+  end
+
+  def unconfigure_icon(user)
+    visit edit_user_registration_path
+    within ".user-edit-form" do
+      fill_in "名前", with: user.name
+      check "アイコン画像をデフォルトに戻す"
+      fill_in "メールアドレス", with: user.email
+      fill_in "新しいパスワード", with: ""
+      fill_in "新しいパスワード（確認用）", with: ""
+      fill_in "現在のパスワード", with: user.password
+      click_on "更新"
+    end
+  end
+end
