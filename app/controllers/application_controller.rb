661a5fea0b5334f320b92a0115d70e1c7570054c
西山仁隆 <jinryu@NishiyamaJinryu-MacBookPro.local>
2022-01-19 17:28:09 +0900

    Add: ゲストユーザーのページ遷移を制限するメソッドを追加
    ゲストユーザーがユーザー情報の編集をできないようにするため、追加しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :exclude_guest_user, if: :devise_controller?, only: :edit
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :icon, :remove_icon])
+  end
+
+  def exclude_guest_user
+    if current_user.email == "guest@example.com"
+      redirect_to root_path, alert: "ゲストユーザーの情報は変更できません"
+    end
   end
 end
