6046e347f712342b3db5fc3c1f03008faa8fba6e
西山仁隆 <jinryu@NishiyamaJinryu-MacBookPro.local>
2022-01-22 16:29:41 +0900

    Add: アクションを追加
    gamesテーブルに新しいレコードを作成できるようにするため、追加しました。

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,24 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to root_path, notice: "新しいページを作成しました"
+    else
+      flash.now[:alert] = "ページを作成できませんでした"
+      render "new"
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :purpose, :description)
+  end
 end
