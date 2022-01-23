8f5565672da342701650e1ef63691d35bd9684f9
西山仁隆 <jinryu@NishiyamaJinryu-MacBookPro.local>
2022-01-24 00:06:51 +0900

    Add: テストデータを追加
    テストで必要になるため、usersテーブルとの紐付けと無効な値を持ったデータの定義を行いました。

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :game do
+    association :user
     name { "テストゲーム" }
     purpose { "テスト" }
     description { "テスト用説明文です。" }
+
+    trait :invalid do
+      name { nil }
+    end
   end
 end
