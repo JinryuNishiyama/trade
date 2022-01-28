414bda69ff60e2bb3c4b5382495f944e642b7005
西山仁隆 <jinryu@NishiyamaJinryu-MacBookPro.local>
2022-01-28 17:07:53 +0900

    Update: ユーザー編集処理を行う関数の内容を変更
    関数の変数を二つにし、アイコンの登録と削除で使い分けられるようにするため、変更しました。

--- spec/support/edit_user_module.rb
+++ spec/support/edit_user_module.rb
@@ -1,22 +1,13 @@
 module EditUserModule
-  def configure_icon(user)
+  def edit(user, icon_setting)
     visit edit_user_registration_path
     within ".user-edit-form" do
       fill_in "名前", with: user.name
-      attach_file "アイコン画像", "#{Rails.root}/spec/factories/test.jpg"
-      fill_in "メールアドレス", with: user.email
-      fill_in "新しいパスワード", with: ""
-      fill_in "新しいパスワード（確認用）", with: ""
-      fill_in "現在のパスワード", with: user.password
-      click_on "更新"
-    end
-  end
-
-  def unconfigure_icon(user)
-    visit edit_user_registration_path
-    within ".user-edit-form" do
-      fill_in "名前", with: user.name
-      check "アイコン画像をデフォルトに戻す"
+      if icon_setting == "add_icon"
+        attach_file "アイコン画像", "#{Rails.root}/spec/factories/test.jpg"
+      else
+        check "アイコン画像をデフォルトに戻す"
+      end
       fill_in "メールアドレス", with: user.email
       fill_in "新しいパスワード", with: ""
       fill_in "新しいパスワード（確認用）", with: ""
