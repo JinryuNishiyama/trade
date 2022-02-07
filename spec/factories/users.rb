eab331c0b4d2a2d40b9bd7dfc64b0ee92483e6cb
西山仁隆 <jinryu@NishiyamaJinryu-MacBookPro.local>
2022-02-07 13:51:33 +0900

    Add: 無効なパラメータを持つuserのテストデータを追加
    テストファイル上でのuserの定義を簡潔に記述できるようにするため、追加しました。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,10 @@ FactoryBot.define do
     sequence(:email) { |n| "test#{n}@example.com" }
     password { "testuser" }
 
+    trait :invalid do
+      email { nil }
+    end
+
     trait :with_icon do
       icon { Rack::Test::UploadedFile.new("#{Rails.root}/spec/factories/test.jpg", "image/jpeg") }
     end
