7b9188a3c903beb70817ba1f30723856e3b65bfc
西山仁隆 <jinryu@NishiyamaJinryu-MacBookPro.local>
2022-02-07 13:49:51 +0900

    Add: ユーザー登録モジュールを使用するための設定を追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,6 +64,7 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include SignUpModule
   config.include SignInModule
   config.include EditUserModule
   config.include FillOutFormModule
