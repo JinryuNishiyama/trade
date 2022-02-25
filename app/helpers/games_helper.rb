b2bfa24b69d828744b9d8738c871800fbec3e36b
西山仁隆 <jinryu@NishiyamaJinryu-MacBookPro.local>
2022-02-25 18:20:54 +0900

    Fix: 長すぎる文を複数行に分割
    見やすいコードにするために、修正しました。

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -3,7 +3,9 @@ module GamesHelper
     reply_to = text.slice(/>>[0-9]+/)
     if reply_to
       reply_to_num = reply_to.slice(/[0-9]+/)
-      link = link_to reply_to, game_path(game, anchor: "chat-list-item-#{reply_to_num}"), class: "chat-reply-link"
+      link = link_to reply_to,
+        game_path(game, anchor: "chat-list-item-#{reply_to_num}"),
+        class: "chat-reply-link"
     end
     content = text.sub(/>>[0-9]+/, "")
     [link: link, content: content]
