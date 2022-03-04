e2d63b674b4e86dee3a263361e2004792e47a12e
西山仁隆 <jinryu@NishiyamaJinryu-MacBookPro.local>
2022-03-04 17:41:22 +0900

    Add: 管理画面へのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,14 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
+
+  namespace :admin do
+    resources :users
+    resources :games
+    resources :posts
+
+    root 'users#index'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
