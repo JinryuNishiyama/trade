bcbecf463269f734c696213de37a3bb1e212f5e2
西山仁隆 <jinryu@NishiyamaJinryu-MacBookPro.local>
2022-03-10 21:49:41 +0900

    Add: ユーザー名に文字数制限を付与
    ユーザー名が長くなりすぎるのを防ぐために追加しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 16 }
   validates :admin, inclusion: { in: [true, false] }
 
   mount_uploader :icon, ImageUploader
