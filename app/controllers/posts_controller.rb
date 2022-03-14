0eaa6f90fa3550a59816bb089bbb4f87ea0309f5
西山仁隆 <jinryu@NishiyamaJinryu-MacBookPro.local>
2022-03-14 16:53:23 +0900

    Add: chat_numカラムをストロングパラメータに設定
    データベースに登録できるようにするため、追加しました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def create
     @game = Game.find(params[:game_id])
-    @posts = @game.posts.includes(:user)
+    @posts = @game.posts.includes(:user).order("created_at desc")
     @post = Post.new(post_params)
 
     if @post.save
@@ -15,6 +15,8 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:text).merge(user_id: current_user.id, game_id: @game.id)
+    params.require(:post).permit(:text).merge(
+      user_id: current_user.id, game_id: @game.id, chat_num: @posts.count + 1
+    )
   end
 end
