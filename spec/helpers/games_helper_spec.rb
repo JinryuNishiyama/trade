a536d472c91b3da88b3719ea96b1b54c886240dc
JinryuNishiyama <jinryunishiyama@gmail.com>
2022-03-25 11:20:03 +0900

    Add: keyword(*columns)メソッドのテストを追加
    検索ワードの取得が正しくできているか確かめるために、追加しました。

--- spec/helpers/games_helper_spec.rb
+++ spec/helpers/games_helper_spec.rb
@@ -1,6 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe GamesHelper, type: :helper do
+  describe "keyword(*columns)" do
+    subject { keyword(*columns) }
+
+    context "params[:q]がnilの場合" do
+      let(:columns) { [:column] }
+      let(:params) do
+        { q: nil }
+      end
+
+      it { is_expected.to eq "" }
+    end
+
+    context "params[:q]が存在する場合" do
+      context "columnsの要素が1つである場合" do
+        let(:columns) { [:column] }
+
+        context "params[:q]の要素の値が空白の場合" do
+          let(:params) do
+            { q: { column: "" } }
+          end
+
+          it { is_expected.to eq "" }
+        end
+
+        context "params[:q]の要素の値が存在する場合" do
+          let(:params) do
+            { q: { column: "ゲーム名" } }
+          end
+
+          it { is_expected.to eq "ゲーム名" }
+        end
+      end
+
+      context "columnsの要素が複数ある場合" do
+        let(:columns) { [:column1, :column2] }
+
+        context "params[:q]の要素のうち、値がnilのものがある場合" do
+          let(:params) do
+            { q: { column1: nil, column2: "ジャンル" } }
+          end
+
+          it { is_expected.to eq "ジャンル" }
+        end
+
+        context "params[:q]の要素のうち、値がnilのものがない場合" do
+          let(:params) do
+            { q: { column1: "ゲーム名", column2: "ジャンル" } }
+          end
+
+          it { is_expected.to eq "ゲーム名,ジャンル" }
+        end
+      end
+    end
+  end
+
   describe "generate_link(text, game)" do
     subject { generate_link(text, game) }
 
